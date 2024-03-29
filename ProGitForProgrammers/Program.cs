aea2f7413d40901d30885cfc63f08c10d6170a5a
Jonathan Garcia <jonathangrcr@gmail.com>
2024-03-29 16:09:26 -0600

    Add write indicating we are in the command line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in JetBrains Rider");
+Console.WriteLine("I just added this in Command Line repo");
