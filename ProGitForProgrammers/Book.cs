4339cffe0895363830b3e99510f5482c1788fd29
Jonathan Garcia <jonathangrcr@gmail.com>
2024-03-30 14:26:37 -0600

    Added the book class

---
+++ ProGitForProgrammers/Book.cs
@@ -0,0 +1,8 @@
+namespace ProGitForProgrammers;
+
+public class Book
+{
+    public string Title { get; set; }
+    public List<string> Autors { get; set; }
+    public DateOnly PublicationDate { get; set; }
+}
