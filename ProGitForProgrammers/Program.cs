fbd6f582ac9b99dd39b9b7529327fb605e703924
Jonathan Garcia <jonathangrcr@gmail.com>
2024-03-30 13:30:41 -0600

    My message

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -2,5 +2,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in JetBrains Rider");
 Console.WriteLine("I just added this in Command Line repo");
+Console.WriteLine("This line added to show the staging area");
 
 // We are going to test commit messages
